684e09880859720e79ced0cfe050e97f3d6b2dac
Yuriy Emelin <yuem-1@yandex.ru>
2023-07-05 23:29:49 +0300

    Решил задачу 29

---
+++ Task29/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача №29 
+// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран. 
+
+// Решение задачи:
+
+ //Метод читает данные от пользователя
+ int ReadData(string msg)
+ {
+     Console.WriteLine(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
+
+ int[] Gen1DArr(int num, int begin, int end)
+ {
+     Random rnd = new Random();
+     int[] arr = new int[num];
+     for (int i = 0; i < arr.Length; i++)
+     {
+         arr[i] = rnd.Next(begin, end);
+     }
+     return arr;
+ }
+
+ void Print1DArr(int[] arr)
+ {
+     Console.Write("[");
+     for (int i = 0; i < arr.Length-1; i++)
+     {
+         Console.Write(arr[i]+", ");
+     }
+     Console.WriteLine(arr[arr.Length-1]+"]");
+ }
+
+ int arrLen = ReadData("Введите длину массива: ");
+ int arrFirst = ReadData("Введите левый диапазон эл-тов: ");
+ int arrEnd = ReadData("Введите правый диапазон эл-тов: ");
+
+ int[] arr = Gen1DArr(arrLen, arrFirst, arrEnd);
+
+ Print1DArr(arr);
+
